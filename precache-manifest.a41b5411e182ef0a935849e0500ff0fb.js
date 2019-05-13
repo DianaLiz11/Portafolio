685dec5ae021e05f1f494cb5d9ba969162e0d095
DianaLiz11 <dianamunoz86@live.com.mx>
2019-05-13 12:17:28 -0500

    Updates

---
+++ precache-manifest.a41b5411e182ef0a935849e0500ff0fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfd7a26ef7980f050e54b051cb5722e",
+    "url": "/Portafolio/index.html"
+  },
+  {
+    "revision": "6703e8f8a9427ac6c311",
+    "url": "/Portafolio/static/css/main.c052887c.chunk.css"
+  },
+  {
+    "revision": "526f1df077e683c38fc1",
+    "url": "/Portafolio/static/js/2.9aeef01b.chunk.js"
+  },
+  {
+    "revision": "6703e8f8a9427ac6c311",
+    "url": "/Portafolio/static/js/main.62046334.chunk.js"
+  },
+  {
+    "revision": "0dceec934d17b8d03d71",
+    "url": "/Portafolio/static/js/runtime~main.9747bc56.js"
+  }
+]);
